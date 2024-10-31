30c7d64a43eb966429cd7c593bc03f3ab9fa83e1
ingvr94 <ingvar94@mail.ru>
2024-10-31 21:48:58 +0300

    cors

--- dist/index.js
+++ dist/index.js
@@ -4,12 +4,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
-const port = 4040;
-app.get('/', (req, res) => {
-    res.send('Express Typescript on Vercel');
-});
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    return console.log(`Server is listening on ${port}`);
+    console.log(`Server running at http://localhost:${port}`);
+});
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+app.all('/*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Header', 'X-Requested-With');
+    res.header('Access-Control-Allow-Methods', 'POST');
+    next();
+});
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send(`Thank you for your interest, ${req.body.name}`);
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
-import express from 'express';
-import { Express, Request, Response} from 'express';
+import express, {Express, NextFunction, Request, Response } from 'express';
+import cors from 'cors'
 
-const app: Express = express()
-const port: Number = 4040;
-
-
-app.get('/', (req: Request, res: Response)=>{
-    res.send('Express Typescript on Vercel')
-})
+const app:Express = express();
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    return console.log(`Server is listening on ${port}`)
-  })
+ console.log(`Server running at http://localhost:${port}`);
+});
+
+ app.use(express.json())
+ app.use(cors<Request>())
+ app.all('/*',function(req: Request, res: Response,next:NextFunction) {
+     res.header('Access-Control-Allow-Origin','*')
+     res.header('Access-Control-Allow-Header','X-Requested-With')
+     res.header('Access-Control-Allow-Methods','POST')
+     next()
+ })
+
+ app.post('/', (req: Request, res: Response) => {
+    
+     console.log(req.body)
+     res.send(`Thank you for your interest, ${req.body.name}`);
+   });
